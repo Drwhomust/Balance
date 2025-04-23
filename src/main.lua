d3b7ed8185dc6725ba229f1b8eca034f10a0f4ed
Drwhomust <rabbids8008@gmail.com>
2025-04-22 17:36:38 -0700

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -12,5 +12,5 @@ function love.draw()
    love.graphics.print(debugnumber, 400, 300)
   end
 
-  love.graphics.rectangle("fill", 20,50, 60,120)
+  love.graphics.rectangle("fill", 0,200, 60,120)
 end
