74b4f802dc856ba9121d896f311a47efd404de80
Drwhomust <rabbids8008@gmail.com>
2025-04-22 17:50:32 -0700

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,18 +1,19 @@
 function love.load()
   debug = 1
   debugnumber = 1
+
+  love.graphics.setBackgroundColor(39, 132, 2)
 end
 
 function love.update(dt)
   debugnumber = debugnumber + 1
 end
 
-love.graphics.setBackgroundColor(39, 132, 2)
-
 function love.draw()
   if debug == 1 then
    love.graphics.print(debugnumber, 400, 300)
   end
 
+  love.graphics.setBackgroundColor(159, 160, 159)
   love.graphics.rectangle("fill", 0,480, 800,120)
 end
